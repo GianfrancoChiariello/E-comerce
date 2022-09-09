49aa5d3dd0cea424c6b25599f6a579321c983039
Andromeda-h <gian.chiariello@gmail.com>
2022-09-09 11:51:20 -0300

    fix

--- app.js
+++ app.js
@@ -67,64 +67,54 @@ fetch("../js/data.json")
         productos.forEach(producto => {
 
             if (producto.producto.includes(search.value)) {
-                let div = document.createElement("div")
-                div.classList = "div"
-                div.innerHTML = `<div>
-                        <img src=${producto.imgUrl}>
-                        <h1>${producto.producto}</h1>
-                        <p>${producto.precio}</p>
-                        <p>Categoria:${producto.categoria}</p>
-                        <button class="btns" id=${producto.id}>Agregar al Carrito</button>
-                        </div>`
-                container.append(div)
+                pintar(producto)
             }
         });
+    }
 
-        console.log(productos)
+    let filter = document.getElementById("filter")
+    filter.addEventListener("click",filterFunction)
 
-        let filter = document.getElementById("filter")
-        filter.addEventListener("click",filterFunction)
+    function filterFunction(e) {
+        container.innerHTML = ""
+        let productosOrdenados = productos.sort((a,b) => a.precio - b.precio)
+        productosOrdenados.forEach(producto => {
+            pintar(producto)
+        });
+    }
 
-        function filterFunction() {
+
+
+    // funciones 
+
+    function agregarProductos(e) {
+        let agregar = productos.find(producto => producto.id == e.target.id)
         
-            container.innerHTML = ""
-            
-            let pf = productos.sort(((a , b) => a.precio - b.precio))
-    
-    
-            pf.forEach(producto => {
-                let div = document.createElement("div")
-                div.innerHTML = `<div>
-                        <img src=${producto.imgUrl}>
-                        <h1>${producto.producto}</h1>
-                        <p>${producto.precio}</p>
-                        <p>Categoria:${producto.categoria}</p>
-                        <button class="btns" id=${producto.id}>Agregar al Carrito</button>
-                        </div>`
-                container.append(div)
-            })
-        }
+        let div2 = document.createElement("div")
+        div2.classList = "div2"
+
+        div2.innerHTML = `<div>
+                            <h1>Producto Agregado: ${agregar.producto},Valor:${agregar.precio}</h1>
+                            </div>`
+        containerCheck.append(div2)
+    }
+
+    function pintar(producto) {
+        let div = document.createElement("div")
+        div.classList = "div"
+        div.innerHTML = `<div>
+                <img src=${producto.imgUrl}>
+                <h1>${producto.producto}</h1>
+                <p>${producto.precio}</p>
+                <p>Categoria:${producto.categoria}</p>
+                <button class="btns" id=${producto.id}>Agregar al Carrito</button>
+                </div>`
+        container.append(div)
 
         let btn = document.querySelectorAll(".btns")
         btn.forEach(botones => {
             botones.addEventListener("click",agregarProductos)
         })
-    
-        function agregarProductos(e) {
-            let agregar = productos.find(producto => producto.id == e.target.id)
-            
-            let div2 = document.createElement("div")
-            div2.classList = "div2"
-    
-            div2.innerHTML = `<div>
-                                <h1>Producto Agregado: ${agregar.producto},Valor:${agregar.precio}</h1>
-                                </div>`
-            containerCheck.append(div2)
-
-            console.log(productos)
-        }
-
-
     }
 
 
