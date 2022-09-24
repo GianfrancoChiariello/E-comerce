aa9b3d8bfe7e803112fc77bc799fb01515f7d60c
Andromeda-h <gian.chiariello@gmail.com>
2022-09-24 16:36:26 -0300

    Pre final

--- app.js
+++ app.js
@@ -133,11 +133,11 @@ getData().then(productos => {
  */
 
 
-fetch("./js/data.json")
+/* fetch("./js/data.json")
 .then(response => response.json())
 .then(productos => {
     
-    window.addEventListener("load", localRestore)
+    
     const search = document.getElementById("search1")
     search.addEventListener("input",chargeProductDom)
     const container = document.getElementById("container")
@@ -151,6 +151,7 @@ fetch("./js/data.json")
     chargeProductDom()
     totalCart()
     filterProducts()
+    localRestore()
 
     function localRestore() {
         if (localStorage.getItem("cart") == null) {
@@ -269,3 +270,290 @@ fetch("./js/data.json")
         })
     }
 }) 
+ */
+
+
+/* fetch("./js/data.json")
+.then(response => response.json())
+.then(productos => {
+    let search = document.getElementById("search1")
+    search.addEventListener("input",loadProducts)
+    let container = document.getElementById("container")
+    let containerCheck = document.getElementById("containerCheck")
+    let containerTotal = document.getElementById("containerTotal")
+    let cart = []
+
+    if (localStorage.getItem("cart") == null) {
+        localStorage.setItem("cart",cart)
+    } else {
+        cart = JSON.parse(localStorage.getItem("cart"))
+    }
+
+    loadProducts()
+    loadCart()
+    totalPrice()
+
+    function loadProducts() {
+        container.innerHTML = ""
+        productos.forEach(producto => {
+            if (producto.producto.includes(search.value)) {
+                createDom(producto)
+            }
+        });
+    }
+
+    function createDom(producto) {
+        let div = document.createElement("div")
+        div.innerHTML = `<div>
+                            <img src=${producto.imgUrl}>
+                            <h1>${producto.producto}</h1>
+                            <p>${producto.precio}</p>
+                            <button class="btns" id=${producto.id}>Add</button>
+                            </div>`
+        container.append(div)
+
+        let btns = document.querySelectorAll(".btns")
+        btns.forEach(btn => {
+            btn.addEventListener("click",addProduct)
+        })
+    }
+
+    function addProduct(e) {
+        let add = productos.find(producto => producto.id == e.target.id)
+        cart.push(add)
+
+        localStorage.setItem("cart",JSON.stringify(cart))
+
+        loadCart()
+        totalPrice()
+    }
+
+    function loadCart() {
+        containerCheck.innerHTML = ""
+        
+        cart.forEach(productos => {
+            let div2 = document.createElement("div")
+            div2.innerHTML = `<div>
+                                <h1>${productos.producto}</h1>
+                                <p>${productos.precio}</p>
+                                <button class="btnX" id=${productos.id}>X</button>
+                                </div>`
+            containerCheck.append(div2)
+        }) 
+
+        let btnX = document.querySelectorAll(".btnX")
+        btnX.forEach(btn => {
+            btn.addEventListener("click",deleteProduct)
+        })
+    }
+
+    function deleteProduct(e) {
+        let delet = cart.find(producto => producto.id == e.target.id)
+        cart.splice(cart.indexOf(delet),1)
+
+        localStorage.setItem("cart",JSON.stringify(cart))
+
+        loadCart()
+        totalPrice()
+    }
+
+    function totalPrice() {
+        let total = cart.map(producto => producto.precio)
+
+        let totalPrice = 0
+
+        total.forEach(pr => {
+            totalPrice += pr
+        })
+
+        containerTotal.innerHTML = `<h1>Total:$${ totalPrice}</h1>`
+    }
+
+
+}) */
+
+
+
+
+fetch('./js/data.json')
+.then(response => response.json())
+.then(productos => {
+    
+    let cart = []
+    let search = document.getElementById("search1")
+    search.addEventListener("input",loadProducts)
+    let container = document.getElementById("container")
+    let containerCheck = document.getElementById("containerCheck")
+    let containerTotal = document.getElementById("containerTotal")
+    let selects = document.querySelectorAll(".select")
+    let deleteAll = document.querySelector(".deleteAll")
+    deleteAll.addEventListener("click",function() {
+        cart = []
+        loadProducts()
+        loadCart()
+        totalCart()
+        localStorage.setItem("cart",JSON.stringify(cart))
+    })
+    
+
+    if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"))
+        loadProducts()
+        loadCart()
+        totalCart()
+    }
+
+    loadProducts()
+    totalCart()
+    filterProducts()
+
+    function loadProducts() {
+        container.innerHTML = ""
+
+        productos.forEach(producto => {
+            if (producto.producto.includes(search.value)) {
+                loadDom(producto)
+            }
+        });
+    }
+
+    function loadDom(producto) {
+        let div = document.createElement("div")
+        div.innerHTML = `<div>
+                            <img src=${producto.imgUrl}>
+                            <h1>${producto.producto}</h1>
+                            <p>$${producto.precio}</p>
+                            <p>Stock:${producto.stock}</p>
+                            <input class="inputS" placeholder="1" min="1" value="1"  type="number">
+                            <button class="btnS" id=${producto.id}>Add</button>
+                            </div>`
+        container.append(div)
+
+        let btnS = document.querySelectorAll(".btnS")
+        btnS.forEach(btn => {
+            btn.addEventListener("click",addProduct)
+        })
+
+    }
+
+
+    function addProduct(e) {
+        let add = productos.find(producto => producto.id == e.target.id)
+        let input = e.target.parentElement.querySelector(".inputS").value
+        
+        if(input <= add.stock && cart.includes(add) == false) {
+            cart.push(add)
+            add.stock = add.stock - input
+            add.cantidad = input
+
+            if (input == 1) {
+                add.totalPrice = add.precio
+            } else {
+                add.totalPrice = add.precio * add.cantidad
+            }
+
+            loadCart()
+            totalCart()
+            loadProducts()
+        
+        } else if (add.stock >= 1 && input <= add.stock) {
+            add.cantidad++
+            add.stock = add.stock - input
+            
+            if (add.cantidad >= 0) {
+                add.totalPrice = add.precio * add.cantidad
+            }
+
+            if (add.stock == 0) {
+                alert("No hay mas Stock de " + add.producto)
+            }
+
+            loadCart()
+            totalCart()
+            loadProducts()
+        }
+
+        localStorage.setItem("cart",JSON.stringify(cart))
+    }
+
+    function deleteProduct(e) {
+        let delet = cart.find(producto => producto.id == e.target.id)
+        cart.splice(cart.indexOf(delet),1)
+
+
+        let stock = productos.find(producto => producto.id == e.target.id)
+        stock.stock = parseInt(stock.stock) + parseInt(delet.cantidad)
+
+        loadCart()
+        totalCart()
+        loadProducts()
+
+        localStorage.setItem("cart",JSON.stringify(cart))
+    }
+
+    function loadCart() {
+        containerCheck.innerHTML = ""
+        
+        
+        cart.forEach(producto => {
+            let div2 = document.createElement("div")
+            div2.innerHTML = `<div>
+                                <h1>Producto:${producto.producto},Precio:${producto.precio}</h1>
+                                <p>Unidades:${producto.cantidad}</p>
+                                <button class="btnX" id=${producto.id}>X</button>
+                                </div>`
+            containerCheck.append(div2)
+        })
+
+        let btnX = document.querySelectorAll(".btnX")
+        btnX.forEach(btn => {
+            btn.addEventListener("click",deleteProduct)
+        })
+    }
+
+    function totalCart() {
+        let precios = cart.map(productos => productos.totalPrice)
+        let total = 0
+
+        precios.forEach(tt => {
+            total = total + tt
+        })
+
+
+        if (cart.length == 0) {
+            containerTotal.innerHTML = `No se agrego ningun producto al carrito!`
+        } else if (cart.length >= 0) {
+            containerTotal.innerHTML = `Total:$${total}`
+        }
+    }
+
+    function loadDomFilters(date) {
+        container.innerHTML = ""
+
+        date.forEach(dt => {
+            loadDom(dt)
+        })
+    }
+
+    function filterProducts() {
+        selects.forEach(select => {
+            select.addEventListener("change",function() {
+                let value = select.options[select.selectedIndex].value
+
+                if (value == "Menor a Mayor") {
+                    loadDomFilters(productos.sort((a,b) => a.precio - b.precio))
+                } else if (value == "Mayor a Menor") {
+                    loadDomFilters(productos.sort((a,b) => b.precio - a.precio))
+                } else if (value == "A - Z") {
+                    loadDomFilters(productos.sort((a,b) => a.producto.localeCompare(b.producto)))
+                } else if (value == "Z - A") {
+                    loadDomFilters(productos.sort((a,b) => b.producto.localeCompare(a.producto)))
+                }
+            })
+        })
+    }
+})
+
+.catch(function(errors) {
+    alert(errors)
+})
